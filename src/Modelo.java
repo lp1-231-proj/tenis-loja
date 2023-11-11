f7da3aa3e7037c3be8793f4b2212f9d4e30b4438
leonardosoux <126433909+leonardosoux@users.noreply.github.com>
2023-11-11 12:09:14 -0300

    Update Modelo.java

--- src/Modelo.java
+++ src/Modelo.java
@@ -1,3 +1,59 @@
 public class Modelo {
+
+    static Integer id;
+    private String nome;
+    private String colorway;
+    private String tamanho;
+    private int quantidade;
+
+    public Tenis(Integer id, string nome, string colorway, string tamanho, int quantidade) {
+        Modelo.id = id;
+        this.nome = nome;
+        this.colorway = colorway;
+        this.tamanho = tamanho;
+        this.quantidade = quantidade;
+    }
+    public Tenis () {
+    }
+
+    public static Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        Tenis.id = id;
+    }
+
+    public string getNome(){
+        return nome;
+    }
+
+    public void setNome(string nome){
+        this.nome = nome;
+    }
+
+    public string getColorway(){
+        return colorway;
+    }
+
+    public void setColorway(int colorway){
+        this.colorway = colorway;
+    }
+
+    public string getTamanho(){
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho){
+        this.tamanho = tamanho;
+    }
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
     
 }
