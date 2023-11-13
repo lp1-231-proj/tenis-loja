18cf99f48a30b90528ff1df1a1de59a0ba186495
Maysa <maysavieira003@gmail.com>
2023-11-13 14:18:10 -0300

    redefinir senha

--- src/RedefinirSenha.java
+++ src/RedefinirSenha.java
@@ -1,3 +1,10 @@
 public class RedefinirSenha {
-    
+
+    static Integer id;
+    private static int login_id;
+    private String novaSenha;
+    private boolean redefinido;
+    private String codigoVerificador;
+    private String dataRedefinição;
+
 }
